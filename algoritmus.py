523d17df5cdcbbb6d9c74a794cd54f0d3d06cd62
Denebola <zuzana.bodri@gmail.com>
2026-05-22 21:07:20 +0200

    welcome, read_file

--- algoritmus.py
+++ algoritmus.py
@@ -1,8 +1,15 @@
+"""autor: Zuzana Bodri
+Tento program slúži na filtrovanie chemických prvkov podľa zadaných pravidiel.
+Program načíta zoznam chemických prvkov z JSON súboru, umožní užívateľovi zadať pravidlá a následne vytriedi zoznam podľa týchto pravidiel."""
+
+import json
+
 def welcome():
     print ("Vitajte vo filtri chemických prvkov!")
 
-def read_file():
-    zoznam = []
+def read_file(source):
+    with open(source, encoding = "utf-8") as file:
+        zoznam = json.load(file)
     return zoznam
 
 def define_rules():
@@ -17,4 +24,8 @@ def algoritmus(zoznam, pravidla):
             vytriedeny_zoznam.append(item)
         else:
             pass
-    return f"Vytriedený zoznam podľa pravidiel {pravidla} je: {vytriedeny_zoznam}"
+    print (f"Prvky, ktoré zodpovedajú pravidlám {pravidla} sú:\n {vytriedeny_zoznam}")
+
+
+welcome ()
+read_file ("elements.json")
