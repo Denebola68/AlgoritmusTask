991b44513203278aad9982924da5fe31563882b7
Denebola <zuzana.bodri@gmail.com>
2026-06-05 09:47:58 +0200

    doplnená dokumentácia a readme

--- README.md
+++ README.md
@@ -1,6 +1,17 @@
 # AlgoritmusTask
 
-## Task description:
+Tento repozitár slúži pre študijné účely. 
+Algoritmus vznikol na základe úlohy, viď nižšie :Task description:.
+Ako zoznam som si zvolila volne sťažiteľný JSON dataset chemických prvkov (obsahuje len výber chemických prvkov, nie celý zoznam!): [dostupný tu](https://jsonlint.com/datasets/elements)
+
+## Hlavné ciele:
+- samostatná sekcia pre logiku pravidiel oddelená od samotnej metódy algoritmus
+- dataset oddelený
+- škálovateľnosť pre prípadné doplnenie ďalších pravidiel
+- upevniť si prácu s JSON, cykly while a for, modularita
+- precvičiť si písanie testov (unittest)
+
+### Task description:
 Prosím napište algoritmus (jazyk volný), který ze seznamu vytřídí prvky podle nějakých pravidel, včetně příkladu takového pravidla. Tzn. ať je tam sekce, kam se doplní konkrétní pravidla, prvky budou někde mimo v seznamu, který algoritmus projde a smaže hodnoty, které neprošly pravidly. Prosím o instrukci jak spustit a kde najdeme algoritmus.
 
 ## Instructions:
--- test_algoritmus.py
+++ test_algoritmus.py
@@ -1,8 +1,11 @@
+# importy:
 from unittest import TestCase
 from algoritmus import rule_phase_match, algoritmus
 
 
 class TestRulePhaseMatch(TestCase):
+
+    # microslovník pre testovanie funkcie rule_phase_match
     item = {
             "number": 2,
             "symbol": "He",
@@ -12,14 +15,19 @@ class TestRulePhaseMatch(TestCase):
             "phase": "Gas",
             "discovered": 1868
     }
+
+    # test, že funkcia rule_phase_match vracia True, keď sa phase of item (Gas) zhoduje s var phase_choice (Gas)
     def test_phase_match(self):
         self.assertTrue(rule_phase_match(self.item, "Gas"))
-    
+
+    # test, že funkcia rule_phase_match vracia False, keď sa phase of item (Gas) NEzhoduje s var phase_choice (Solid)
     def test_phase_mismatch(self):
         self.assertFalse(rule_phase_match(self.item, "Solid"))
 
     
 class TestAlgoritmus(TestCase):
+    
+    # výrez z JSONu pre testy
     zoznam = {
         "elements": [
     {
@@ -42,11 +50,14 @@ class TestAlgoritmus(TestCase):
     },
         ]
     }
+
+    # test, že funkcia algoritmus vracia zoznam s prvkami, ktoré odpovedajú fáze "Solid"
     def test_algoritmus_solid(self):
             phase_choice = "Solid"
             returned_filtered_elements = algoritmus(self.zoznam, rule_phase_match, phase_choice)
             self.assertIn(phase_choice, returned_filtered_elements[0]["phase"])
 
+    # test, že funkcia algoritmus vracia zoznam s prvkami, ktoré odpovedajú fáze "Gas"
     def test_algoritmus_gas(self):
             phase_choice = "Gas"
             returned_filtered_elements = algoritmus(self.zoznam, rule_phase_match, phase_choice)
